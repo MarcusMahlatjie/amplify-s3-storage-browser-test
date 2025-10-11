6b52c23929afd93b0a6234654b432a14dd39a713
Marcus <105200274+MarcusMahlatjie@users.noreply.github.com>
2025-10-11 18:27:25 +0200

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "loan-optimization-execution-bucket"
+  default     = "bucket-name"
 }
 
 variable "environment" {
